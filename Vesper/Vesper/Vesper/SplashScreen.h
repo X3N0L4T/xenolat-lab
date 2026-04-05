a72d8e434c1fae586744014828f67d2e324906fe
X3N0 <49507911+X3N0L4T@users.noreply.github.com>
2026-04-04 22:21:25 -0500

    Splash screen improvement

--- Vesper/Vesper/Vesper/SplashScreen.h
+++ Vesper/Vesper/Vesper/SplashScreen.h
@@ -24,6 +24,48 @@ namespace Vesper {
 			label->Font = gcnew Drawing::Font("Sego UI Light", 52, FontStyle::Regular);
 			label->AutoSize = true;
 			label->BackColor = Color::Transparent;
+			this->Controls->Add(label);
+
+			timer = gcnew Timer();
+			timer->Interval = 16;
+			timer->Tick += gcnew EventHandler(this, &SplashScreen::OnTick);
+			timer->Start();
+
+			phase = FADE_IN;
+			targetY = 40;
+		}
+
+	private:
+		Label^ label;
+		Timer^ timer;
+		Phase phase;
+		int targetY;
+
+		void OnTick(Object^ sender, EventArgs^ e) {
+			label->Left = (this->ClientSize.Width - label->Width) / 2;
+
+			if (phase == FADE_IN) {
+				this->Opacity += 0.02;
+				label->Top = (this->ClientSize.Height - label->Height) / 2;
+
+				if (this->Opacity >= 1.0) {
+					this->Opacity = 1.0;
+					phase = MOVE_UP;
+				}
+			}
+			else if (phase == MOVE_UP) {
+				if (label->Top > targetY) {
+					label->Top -= 4;
+				} else {
+					label->Top = targetY;
+					phase = DONE;
+					timer->Stop();
+				}
+			}
+		}
+	protected:
+		~SplashScreen() {
+			if (timer != nullptr) timer->Stop();
 		}
 	};
 }
